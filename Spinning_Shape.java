a4df6e0ac25bab91e5fd92754822c7fc82c1ed27
Katie Domke <kaitlynlaurel.domke@calbaptist.edu>
2025-01-15 09:55:30 -0800

    comments and small edit

--- Spinning_Shape.java
+++ Spinning_Shape.java
@@ -7,30 +7,39 @@ public class Spinning_Shape {
         // Donut parameters
         int width = 40;
         int height = 22;
+
+        // Rotation angles about x and z
         double A = 0;
         double B = 0;
+
+        // Scale of shape
         double K1 = 15;
         double K2 = 5;
+
+        // Shape change - Radii
         double R1 = 1;
         double R2 = 2;
+
+        // Resolution & Brightness
         double theta_spacing = 0.07;
         double phi_spacing = 0.02;
         char[] brightness = ".,-°:;=! *#$@".toCharArray();
 
-// Loop to keep the donut spinning
+// Loop to keep the donut spinning - Condition is always true
         while (true) {
-            double[] z = new double[width * height];
-            char[] b = new char[width * height];
+            double[] z = new double[width * height]; // depth of each "pixel" | sacrifice intuition for memory conservation
+            char[] b = new char[width * height]; // brightness of all "pixels" | sacrifice intuition for memory conservtion
 
             for (int i = 0; i < z.length; i++) {
+                // Blacks out the entire screen
                 z[i] = 0;
                 b[i] = ' ';
             }
 
-            for (double theta = 0; theta < 2 * Math.PI; theta +=
-                    theta_spacing) {
-                for (double phi = 0; phi < 2 * Math.PI; phi +=
-                        phi_spacing) {
+            for (double theta = 0; theta < 2 * Math.PI; theta += theta_spacing)
+                {
+                for (double phi = 0; phi < 2 * Math.PI; phi += phi_spacing)
+                {
                     double cosTheta = Math.cos(theta);
                     double sinTheta = Math.sin(theta);
                     double cosPhi = Math.cos(phi);
@@ -39,21 +48,22 @@ public class Spinning_Shape {
                     double sinA = Math.sin(A);
                     double cosB = Math.cos(B);
                     double sinB = Math.sin(B);
+
+                    // Establish the definition of our object in parametric components -> change to another shape
                     double circleX = R2 + R1 * cosTheta;
                     double circleY = R1 * sinTheta;
 
-                    // 3D coordinates after rotation
+                    // 3D coordinates after rotation - Establish motion
                     double x = circleX * (cosB * cosPhi + sinA * sinB * sinPhi) - circleY * cosA * sinB;
-                    double y = circleX * (sinB * cosPhi - sinA *
-                            cosB * sinPhi) + circleY * cosA * cosB;
+                    double y = circleX * (sinB * cosPhi - sinA * cosB * sinPhi) + circleY * cosA * cosB;
                     double zPos = K2 + cosA * circleX * sinPhi + circleY * sinA;
                     double ooz = 1 / zPos; // One over Z (depth)
+                    // Establish x and y components
                     int xp = (int) (width / 2 + K1 * ooz * x);
                     int yp = (int) (height / 2 - K1 * ooz * y);
                     int idx = xp + yp * width;
 
-                    double luminance = cosPhi * cosTheta * sinB -
-                            cosA * cosTheta * sinPhi - sinA * sinTheta + cosB * (cosA * sinTheta -
+                    double luminance = cosPhi * cosTheta * sinB - cosA * cosTheta * sinPhi - sinA * sinTheta + cosB * (cosA * sinTheta -
                             cosTheta * sinPhi * sinA);
                     if (idx >= 0 && idx < width * height && ooz >
                             z[idx]) {
@@ -66,7 +76,7 @@ public class Spinning_Shape {
                 }
             }
 // Print frame
-            for (int k = 0; k < width * height++) {
+            for (int k = 0; k < width * height; k++) {
                 System.out.print(k % width != 0 ? b[k] : "\n");
             }
 
